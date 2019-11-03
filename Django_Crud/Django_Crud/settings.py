bc21e4b00e1cbea46ed1e8fb0fa33174ca3bb3ba
Cesar Mejia <cesarsac@outlook.com>
2019-11-03 13:16:51 -0600

    se agredo css y se  inserto datos a la base sde datos

--- Django_Crud/Django_Crud/settings.py
+++ Django_Crud/Django_Crud/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'CRUD'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
