e1641b686577559d7eb49e01944745f9ddd89ff2
Cesar Mejia <cesarsac@outlook.com>
2019-11-03 13:00:15 -0600

    se agredo un usuario en la pagina de django

--- Django_Crud/CRUD/admin.py
+++ Django_Crud/CRUD/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import BookList
 
 # Register your models here.
+admin.site.register(BookList)
--- Django_Crud/CRUD/models.py
+++ Django_Crud/CRUD/models.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class BookList(models.Model):
+    title = models.CharField(max_length= 150)
+    price = models.IntegerField()
+    author = models.CharField(max_length=150)
